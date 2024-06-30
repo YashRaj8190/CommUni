60fb2422bf949e90847e65a40b66641a78b96b91
YashRaj8190 <yashraj292004@gmail.com>
2024-06-30 13:08:58 +0530

    Created Send Message api for chats

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const ConnectDB = require("./config/db");
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
 dotenv.config();
+const messageRoutes = require('./routes/messageRoutes');
 const { notFound, errorHandler } = require('./middleware/error');
 const app = express();
 const port = 5000;
@@ -20,6 +21,7 @@ app.get("/", (req, res) => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler)
